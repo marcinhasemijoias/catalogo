47080e1e626feb45f539b3aa462715a1e8e3776e
marcinhasemijoias <marcinhasemijoias18k@gmail.com>
2025-12-21 10:17:50 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open('catalogo-v1').then(cache =>
+cache.addAll([
+'./',
+'./index.html',
+'./admin.html'
+])
+)
+);
+});
+
+
+self.addEventListener('fetch', e => {
+e.respondWith(
+caches.match(e.request).then(r => r || fetch(e.request))
+);
+});
